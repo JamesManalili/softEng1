e04c566d4c9dc32bc321716268315a0edf60bdd2
JamesManalili <142465145+JamesManalili@users.noreply.github.com>
2023-08-17 10:07:13 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String [] args){
+    System.out.println("Hello, Software Engineer Course!");
+     System.out.println("My name is MANALILI, James!");  
+  }
+}
