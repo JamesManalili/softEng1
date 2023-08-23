e79fdc5afcc131fdf9b12a0a6034ec64d1a61626
JamesManalili <142465145+JamesManalili@users.noreply.github.com>
2023-08-23 15:29:12 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,5 @@ public class Hello{
   public static void main(String [] args){
     System.out.println("Hello, Software Engineer Course!");
      System.out.println("My name is MANALILI, James!"); 
-     System.out.println ("Complicated world of SoftDev that is why it pays much!"); 
   }
 }
